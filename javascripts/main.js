6f001e7d5ceb18e43327e27e9acc616681aa9100
Omar Ayyub <obayyub@gmail.com>
2014-08-12 17:39:02 -0400

    updated index

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,6 @@ myApp = angular.module('myApp', [])
 	.controller('MyCtrl', ['$scope', '$http', function($scope, $http){
 		$http({method: 'GET', url: './javascripts/organicacids.json'}).
     		success(function(data, status, headers, config) {
-    			console.log(data);
       			$scope.organicAcids = data;
       			console.log($scope.organicAcids)
     		}).
