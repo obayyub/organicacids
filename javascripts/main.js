3f9a522e61be1b294a6debd401a4f955d6e0067c
Omar Ayyub <obayyub@gmail.com>
2014-08-12 17:30:15 -0400

    updated index

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
-console.log('This would be the main JS file.');
+myApp = angular.module('myApp', [])
+	.controller('MyCtrl', ['$scope', '$http', function($scope, $http){
+		$http({method: 'GET', url: './javascripts/organicacids.json'}).
+    		success(function(data, status, headers, config) {
+    			console.log(data);
+      			$scope.organicAcids = data;
+    		}).
+    		error(function(data, status, headers, config) {
+    			console.log('couldnt grab json');
+    		});
+	}]);
