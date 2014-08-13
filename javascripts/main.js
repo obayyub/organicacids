4ed4bc32d1ca566c01a1209bb2c0798b9f1135f1
Omar Ayyub <obayyub@gmail.com>
2014-08-13 11:46:57 -0400

    added basic search

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,31 @@
-myApp = angular.module('myApp', [])
-	.controller('MyCtrl', ['$scope', '$http', function($scope, $http){
-		$http({method: 'GET', url: './javascripts/organicacids.json'}).
-    		success(function(data, status, headers, config) {
-      			$scope.organicAcids = data;
-      			$scope.boom = [0, 1, 2, 3];
-    		}).
-    		error(function(data, status, headers, config) {
-    			console.log('couldnt grab json');
-    		});
-	}]);
+myApp = angular.module('myApp', []);
+myApp.filter('fuzzyFilter', function () {
+	return function (items, searchText) {
+		var searchWords;
+	
+		if (searchText) {
+			searchWords = searchText.split(' ');
+	
+			return _.filter(items, function (item) {
+				var itemText = _.values(item).join(' ').toLowerCase();
+	
+				return _.every(searchWords, function (searchWord) {
+					return itemText.search(searchWord.toLowerCase()) !== -1;
+				});
+			});
+		} else {
+			return [];
+		}
+	};
+});
+myApp.controller('MyCtrl', ['$scope', '$http', function($scope, $http){
+	$http({method: 'GET', url: './javascripts/organicacids.json'}).
+		success(function(data, status, headers, config) {
+  			$scope.organicAcids = data;
+  			$scope.boom = [0, 1, 2, 3];
+		}).
+		error(function(data, status, headers, config) {
+			console.log('couldnt grab json');
+		});
+}]);
+
